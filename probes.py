797bb9a42f334ca35a8e11d7191fe5b649529867
Maroun Ayli <marounayle@gmail.com>
2020-03-18 14:10:21 +0200

    Added CSGO IPs and Dynamic probes loading is now fixed

---
+++ probes.py
@@ -0,0 +1,52 @@
+dynamic_probes = [
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "LB"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "JO"
+    },
+    {
+        "requested": 1,
+        "type": "country",
+        "value": "SY"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "EG"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "IQ"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "SA"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "BH"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "QA"
+    },
+    {
+        "requested": 2,
+        "type": "country",
+        "value": "AE"
+    },
+    {
+        "requested": 1,
+        "type": "country",
+        "value": "OM"
+    }
+]
